2c4c0ec650f278dd06943f5b1a1fd0c9ebc7fadc
Tanish <b.tanish@gmail.com>
2025-03-04 04:29:30 +0000

    add DiT and Flow training

--- chewbacca/utils/ssv2.py
+++ chewbacca/utils/ssv2.py
@@ -8,26 +8,26 @@ from random import shuffle
 
 os.makedirs("data", exist_ok=True)
 
-with open("./labels/labels.json", "rb") as file:
+with open("/home/ubuntu/datasets/ssv2_2025-03-03_0001/labels/labels.json", "rb") as file:
     lab = json.load(file)
 
-with open("./labels/validation.json", "rb") as file:
+with open("/home/ubuntu/datasets/ssv2_2025-03-03_0001/labels/validation.json", "rb") as file:
     val = json.load(file)
 
-with open("./labels/train.json", "rb") as file:
+with open("/home/ubuntu/datasets/ssv2_2025-03-03_0001/labels/train.json", "rb") as file:
     train = json.load(file)
 
 save_path = "data/ssv2_train_label.npy"
 labels = []
 for i in range(len(train)):
-    file_name = f"/home/ubuntu/datasets/ssv2_2025-03-03_0001/20bn-something-something-v2/{train[i]["id"]}.webm"
+    file_name = f"/home/ubuntu/datasets/ssv2_2025-03-03_0001/20bn-something-something-v2/{train[i]['id']}.webm"
     label = lab[train[i]["template"].replace("[","").replace("]","")]
     labels.append([file_name, label])
 
 save_path = "data/ssv2_val_label.npy"
 labels = []
 for i in range(len(val)):
-    file_name = f"/home/ubuntu/datasets/ssv2_2025-03-03_0001/20bn-something-something-v2/{val[i]["id"]}.webm"
+    file_name = f"/home/ubuntu/datasets/ssv2_2025-03-03_0001/20bn-something-something-v2/{val[i]['id']}.webm"
     label = lab[val[i]["template"].replace("[","").replace("]","")]
     labels.append([file_name, label])
 
