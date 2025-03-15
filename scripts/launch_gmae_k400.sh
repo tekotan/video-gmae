7d0bf6b2a11e96afefa5e68bbd0ca1eaa34b1dda
brjathu <brjathu@gmail.com>
2025-03-15 15:09:35 +0000

    vjepa updates

--- scripts/launch_gmae_k400.sh
+++ scripts/launch_gmae_k400.sh
@@ -1,37 +1,328 @@
 #!/bin/bash
 
+# python chewbacca/train.py -m \
+# --config-name gmae_ema.yaml \
+# hydra/launcher=submitit_slurm \
+# launcher=slurm_x \
+# model._target_="chewbacca.models.gmae.GMAELitModule" \
+# datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
+# task_name=b_vit_base_pretraining_test0 \
+# trainer=ddp_unused \
+# trainer.devices=8 \
+# trainer.num_nodes=8 \
+# configs.inference.testing=True \
+# configs.inference.depth=False \
+# configs.inference.correspondences=False \
+# configs.task="pretrain" \
+# configs.model_name="mae_vit_base_patch16" \
+# configs.input_size=256 \
+# configs.lr=1e-4,1e-3 \
+# configs.weight_decay=5e-2,5e-4 \
+# trainer.accumulate_grad_batches=1 \
+# configs.train_batch_size=8 \
+# configs.test_batch_size=8 \
+# configs.train_num_workers=8 \
+# configs.test_num_workers=8 \
+# trainer.gradient_clip_val=2.0 \
+# configs.scheduler="cosine_step" \
+# configs.lr_interval="step" \
+# configs.warmup_steps=2000 \
+# trainer.max_epochs=400,800 \
+# configs.seq_length=32 \
+# configs.deltas_reg_weight=0.0 \
+# configs.random_frames=True \
+# configs.rgb_deltas=True,False \
+# configs.mean_deltas=True \
+# configs.scale_vocab=1 \
+# configs.sample_rate=1 \
+# configs.num_classes=400 \
+# configs.load_strict=False \
+# configs.dataset_type="video" \
+# trainer.limit_train_batches=1000000 \
+# trainer.limit_val_batches=1000000 \
+# callbacks.model_checkpoint.every_n_epochs=10 \
+# configs.training_type="k400_gaussian_save-images-z_no-mask_random-frames"
+
+
+
+
+
+
+
+
+
+
+
+# python chewbacca/train.py -m \
+# --config-name gmae_ema.yaml \
+# hydra/launcher=submitit_slurm \
+# launcher=slurm_x \
+# model._target_="chewbacca.models.gmae.GMAELitModule" \
+# datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
+# task_name=b_vit_base_pretraining_test1 \
+# trainer=ddp_unused \
+# trainer.devices=8 \
+# trainer.num_nodes=32 \
+# configs.inference.testing=True \
+# configs.inference.depth=False \
+# configs.inference.correspondences=False \
+# configs.task="pretrain" \
+# configs.model_name="mae_vit_base_patch16" \
+# configs.input_size=256 \
+# configs.lr=4e-4 \
+# configs.weight_decay=5e-2 \
+# trainer.accumulate_grad_batches=1 \
+# configs.train_batch_size=2 \
+# configs.test_batch_size=2 \
+# configs.train_num_workers=8 \
+# configs.test_num_workers=8 \
+# trainer.gradient_clip_val=2.0 \
+# configs.scheduler="cosine_step" \
+# configs.lr_interval="step" \
+# configs.warmup_steps=2000 \
+# trainer.max_epochs=400 \
+# configs.seq_length=32 \
+# configs.deltas_reg_weight=0.0 \
+# configs.random_frames=True \
+# configs.rgb_deltas=True \
+# configs.mean_deltas=True \
+# configs.scale_vocab=1 \
+# configs.sample_rate=1 \
+# configs.num_classes=400 \
+# configs.load_strict=False \
+# configs.dataset_type="video" \
+# trainer.limit_train_batches=1000000 \
+# trainer.limit_val_batches=1000000 \
+# callbacks.model_checkpoint.every_n_epochs=10 \
+# configs.training_type="k400_gaussian_save-images-z_no-mask_random-frames"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# python chewbacca/train.py -m \
+# --config-name gmae_ema.yaml \
+# hydra/launcher=submitit_slurm \
+# launcher=slurm_x \
+# model._target_="chewbacca.models.gmae.GMAELitModule" \
+# datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
+# task_name=b_vit_base_pretraining_test2 \
+# trainer=ddp_unused \
+# trainer.devices=8 \
+# trainer.num_nodes=32 \
+# configs.inference.testing=True \
+# configs.inference.depth=False \
+# configs.inference.correspondences=False \
+# configs.task="pretrain" \
+# configs.model_name="mae_vit_base_patch16" \
+# configs.input_size=256 \
+# configs.lr=1e-4 \
+# configs.weight_decay=5e-2 \
+# trainer.accumulate_grad_batches=1 \
+# configs.train_batch_size=2 \
+# configs.test_batch_size=2 \
+# configs.train_num_workers=8 \
+# configs.test_num_workers=8 \
+# trainer.gradient_clip_val=2.0 \
+# configs.scheduler="cosine_step" \
+# configs.lr_interval="step" \
+# configs.warmup_steps=2000 \
+# trainer.max_epochs=400 \
+# configs.seq_length=32 \
+# configs.deltas_reg_weight=0.0 \
+# configs.random_frames=True \
+# configs.rgb_deltas=True \
+# configs.mean_deltas=True \
+# configs.scale_vocab=1 \
+# configs.sample_rate=1 \
+# configs.num_classes=400 \
+# configs.load_strict=False \
+# configs.dataset_type="video" \
+# trainer.limit_train_batches=1000000 \
+# trainer.limit_val_batches=1000000 \
+# callbacks.model_checkpoint.every_n_epochs=10 \
+# configs.training_type="k400_gaussian_save-images-z_no-mask_random-frames"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# python chewbacca/train.py -m \
+# --config-name gmae.yaml \
+# hydra/launcher=submitit_slurm \
+# launcher=slurm_x \
+# model._target_="chewbacca.models.gmae.GMAELitModule" \
+# datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
+# task_name=b_vit_base_pretraining_test3 \
+# trainer=ddp_unused \
+# trainer.devices=8 \
+# trainer.num_nodes=32 \
+# configs.inference.testing=True \
+# configs.inference.depth=False \
+# configs.inference.correspondences=False \
+# configs.task="pretrain" \
+# configs.model_name="mae_vit_base_patch16" \
+# configs.input_size=256 \
+# configs.lr=1e-4 \
+# configs.weight_decay=5e-2 \
+# trainer.accumulate_grad_batches=1 \
+# configs.train_batch_size=2 \
+# configs.test_batch_size=2 \
+# configs.train_num_workers=8 \
+# configs.test_num_workers=8 \
+# trainer.gradient_clip_val=2.0 \
+# configs.scheduler="cosine_step" \
+# configs.lr_interval="step" \
+# configs.warmup_steps=2000 \
+# trainer.max_epochs=400 \
+# configs.seq_length=32 \
+# configs.deltas_reg_weight=0.0 \
+# configs.random_frames=True \
+# configs.rgb_deltas=True \
+# configs.mean_deltas=True \
+# configs.scale_vocab=1 \
+# configs.sample_rate=1 \
+# configs.num_classes=400 \
+# configs.load_strict=False \
+# configs.dataset_type="video" \
+# trainer.limit_train_batches=1000000 \
+# trainer.limit_val_batches=1000000 \
+# callbacks.model_checkpoint.every_n_epochs=10 \
+# configs.training_type="k400_gaussian_save-images-z_no-mask_random-frames"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# python chewbacca/train.py -m \
+# --config-name gmae_ema.yaml \
+# hydra/launcher=submitit_slurm \
+# launcher=slurm_x \
+# model._target_="chewbacca.models.gmae.GMAELitModule" \
+# datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
+# task_name=b_vit_base_pretraining_test4 \
+# trainer=ddp_unused \
+# trainer.devices=8 \
+# trainer.num_nodes=32 \
+# configs.inference.testing=True \
+# configs.inference.depth=False \
+# configs.inference.correspondences=False \
+# configs.task="pretrain" \
+# configs.model_name="mae_vit_base_patch16" \
+# configs.input_size=256 \
+# configs.lr=4e-4 \
+# configs.weight_decay=5e-2 \
+# trainer.accumulate_grad_batches=1 \
+# configs.train_batch_size=2 \
+# configs.test_batch_size=2 \
+# configs.train_num_workers=8 \
+# configs.test_num_workers=8 \
+# trainer.gradient_clip_val=2.0 \
+# configs.scheduler="cosine_step" \
+# configs.lr_interval="step" \
+# configs.warmup_steps=2000 \
+# trainer.max_epochs=400 \
+# configs.seq_length=32 \
+# configs.deltas_reg_weight=0.0 \
+# configs.random_frames=True \
+# configs.rgb_deltas=True \
+# configs.mean_deltas=True \
+# configs.scale_vocab=1 \
+# configs.sample_rate=1 \
+# configs.num_classes=400 \
+# configs.load_strict=False \
+# configs.dataset_type="video" \
+# trainer.limit_train_batches=1000000 \
+# trainer.limit_val_batches=1000000 \
+# callbacks.model_checkpoint.every_n_epochs=10 \
+# configs.training_type="k400_gaussian_save-images-z_no-mask_random-frames" \
+# configs.mask_ratio=0.95
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 python chewbacca/train.py -m \
 --config-name gmae_ema.yaml \
 hydra/launcher=submitit_slurm \
 launcher=slurm_x \
 model._target_="chewbacca.models.gmae.GMAELitModule" \
 datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
-task_name=b_vit_base_pretraining_test0 \
+task_name=b_vit_base_pretraining_test5 \
 trainer=ddp_unused \
 trainer.devices=8 \
-trainer.num_nodes=8 \
+trainer.num_nodes=32 \
 configs.inference.testing=True \
 configs.inference.depth=False \
 configs.inference.correspondences=False \
 configs.task="pretrain" \
 configs.model_name="mae_vit_base_patch16" \
 configs.input_size=256 \
-configs.lr=1e-4,1e-3 \
-configs.weight_decay=5e-2,5e-4 \
+configs.lr=4e-4 \
+configs.weight_decay=5e-2 \
 trainer.accumulate_grad_batches=1 \
-configs.train_batch_size=8 \
-configs.test_batch_size=8 \
+configs.train_batch_size=2 \
+configs.test_batch_size=2 \
 configs.train_num_workers=8 \
 configs.test_num_workers=8 \
 trainer.gradient_clip_val=2.0 \
 configs.scheduler="cosine_step" \
 configs.lr_interval="step" \
 configs.warmup_steps=2000 \
-trainer.max_epochs=400,800 \
+trainer.max_epochs=400 \
 configs.seq_length=32 \
 configs.deltas_reg_weight=0.0 \
 configs.random_frames=True \
-configs.rgb_deltas=True,False \
+configs.rgb_deltas=True \
 configs.mean_deltas=True \
 configs.scale_vocab=1 \
 configs.sample_rate=1 \
@@ -41,4 +332,180 @@ configs.dataset_type="video" \
 trainer.limit_train_batches=1000000 \
 trainer.limit_val_batches=1000000 \
 callbacks.model_checkpoint.every_n_epochs=10 \
-configs.training_type="k400_gaussian_save-images-z_no-mask_random-frames"
+configs.training_type="k400_gaussian_save-images-z_no-mask_random-frames" \
+configs.mask_ratio=0.95
+
+
+
+
+
+
+
+
+
+
+
+
+
+python chewbacca/train.py -m \
+--config-name gmae_ema.yaml \
+model._target_="chewbacca.models.gmae.GMAELitModule" \
+datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
+task_name=b_vit_base_pretraining_test6 \
+trainer=ddp_unused \
+trainer.devices=1 \
+trainer.num_nodes=1 \
+configs.inference.testing=True \
+configs.inference.depth=False \
+configs.inference.correspondences=False \
+configs.task="pretrain" \
+configs.model_name="mae_vit_base_patch16" \
+configs.input_size=256 \
+configs.lr=1e-4 \
+configs.weight_decay=5e-2 \
+trainer.accumulate_grad_batches=1 \
+configs.train_batch_size=2 \
+configs.test_batch_size=2 \
+configs.train_num_workers=8 \
+configs.test_num_workers=8 \
+trainer.gradient_clip_val=2.0 \
+configs.scheduler="cosine_step" \
+configs.lr_interval="step" \
+configs.warmup_steps=2000 \
+trainer.max_epochs=400 \
+configs.seq_length=32 \
+configs.deltas_reg_weight=0.0 \
+configs.random_frames=True \
+configs.rgb_deltas=True \
+configs.mean_deltas=True \
+configs.scale_vocab=1 \
+configs.sample_rate=1 \
+configs.num_classes=400 \
+configs.load_strict=False \
+configs.dataset_type="video" \
+trainer.limit_train_batches=1000000 \
+trainer.limit_val_batches=1000000 \
+callbacks.model_checkpoint.every_n_epochs=10 \
+configs.training_type="k400_gaussian_save-images-z_no-mask_random-frames-attn" \
+configs.mask_ratio=0.95
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+python chewbacca/train.py -m \
+--config-name gmae_ema.yaml \
+hydra/launcher=submitit_slurm \
+launcher=slurm_x \
+model._target_="chewbacca.models.gmae.GMAELitModule" \
+datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
+task_name=b_vit_base_pretraining_test7_32k6 \
+trainer=ddp_unused \
+trainer.devices=8 \
+trainer.num_nodes=32 \
+configs.inference.testing=True \
+configs.inference.depth=False \
+configs.inference.correspondences=False \
+configs.task="pretrain" \
+configs.model_name="mae_vit_base_patch16" \
+configs.input_size=256 \
+configs.lr=1e-4 \
+configs.weight_decay=5e-2 \
+trainer.accumulate_grad_batches=1 \
+configs.train_batch_size=2 \
+configs.test_batch_size=2 \
+configs.train_num_workers=8 \
+configs.test_num_workers=8 \
+trainer.gradient_clip_val=2.0 \
+configs.scheduler="cosine_step" \
+configs.lr_interval="step" \
+configs.warmup_steps=2000 \
+trainer.max_epochs=400 \
+configs.seq_length=32 \
+configs.deltas_reg_weight=0.0 \
+configs.random_frames=True \
+configs.rgb_deltas=True \
+configs.mean_deltas=True \
+configs.scale_vocab=1 \
+configs.sample_rate=1 \
+configs.num_classes=400 \
+configs.load_strict=False \
+configs.dataset_type="video-vjepa" \
+configs.video_source="video-vjepa" \
+trainer.limit_train_batches=1000000 \
+trainer.limit_val_batches=1000000 \
+callbacks.model_checkpoint.every_n_epochs=10 \
+configs.training_type="k400-vjepa_gaussian_save-images-z_no-mask_random-frames-attn" \
+configs.mask_ratio=0.95
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+python chewbacca/train.py -m \
+--config-name gmae_ema.yaml \
+hydra/launcher=submitit_slurm \
+launcher=slurm_x \
+model._target_="chewbacca.models.gmae.GMAELitModule" \
+datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
+task_name=b_vit_base_pretraining_test7_32k7 \
+trainer=ddp_unused \
+trainer.devices=8 \
+trainer.num_nodes=32 \
+configs.inference.testing=True \
+configs.inference.depth=False \
+configs.inference.correspondences=False \
+configs.task="pretrain" \
+configs.model_name="mae_vit_base_patch16" \
+configs.input_size=256 \
+configs.lr=1e-4 \
+configs.weight_decay=5e-2 \
+trainer.accumulate_grad_batches=1 \
+configs.train_batch_size=2 \
+configs.test_batch_size=2 \
+configs.train_num_workers=8 \
+configs.test_num_workers=8 \
+trainer.gradient_clip_val=2.0 \
+configs.scheduler="cosine_step" \
+configs.lr_interval="step" \
+configs.warmup_steps=2000 \
+trainer.max_epochs=400 \
+configs.seq_length=32 \
+configs.deltas_reg_weight=0.0 \
+configs.random_frames=True \
+configs.rgb_deltas=True \
+configs.mean_deltas=True \
+configs.scale_vocab=1 \
+configs.sample_rate=1 \
+configs.num_classes=400 \
+configs.load_strict=False \
+configs.dataset_type="video-vjepa" \
+configs.video_source="video-vjepa" \
+trainer.limit_train_batches=1000000 \
+trainer.limit_val_batches=1000000 \
+callbacks.model_checkpoint.every_n_epochs=10 \
+configs.training_type="k400-vjepa_gaussian_save-images-z_no-mask_random-frames-attn" \
+configs.mask_ratio=0.95
--- scripts/launch_gmae_ucf101.sh
+++ scripts/launch_gmae_ucf101.sh
@@ -8,163 +8,7 @@
 # launcher=slurm_x \
 # model._target_="chewbacca.models.gmae.GMAELitModule" \
 # datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
-# task_name=vit_base_a_pretraining_test1 \
-# trainer=ddp_unused \
-# trainer.devices=8 \
-# trainer.num_nodes=8 \
-# configs.task="pretrain" \
-# configs.model_name="mae_vit_base_patch16" \
-# configs.input_size=224 \
-# configs.lr=1e-4 \
-# configs.weight_decay=5e-2 \
-# trainer.accumulate_grad_batches=1 \
-# configs.train_batch_size=2 \
-# configs.test_batch_size=4 \
-# configs.train_num_workers=16 \
-# configs.test_num_workers=16 \
-# trainer.gradient_clip_val=2.0 \
-# configs.scheduler="cosine_step" \
-# configs.lr_interval="step" \
-# configs.warmup_steps=20 \
-# trainer.max_epochs=800 \
-# configs.seq_length=32 \
-# configs.deltas_reg_weight=0.0 \
-# configs.random_frames=True \
-# configs.rgb_deltas=True \
-# configs.mean_deltas=True \
-# configs.num_classes=174 \
-# configs.vocab_size=256 \
-# configs.dataset_type="video" \
-# trainer.limit_train_batches=50000000 \
-# trainer.limit_val_batches=5000000 \
-# callbacks.model_checkpoint.every_n_epochs=1 \
-# configs.training_type="ucf101_gaussian_save-images-z_no-mask_random-frames" \
-# configs.load_strict=False \
-# configs.mask_ratio=0.95
-
-
-
-
-
-
-
-
-
-
-# python chewbacca/train.py -m \
-# --config-name gmae_ema.yaml \
-# hydra/launcher=submitit_slurm \
-# launcher=slurm_x \
-# model._target_="chewbacca.models.gmae.GMAELitModule" \
-# datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
-# task_name=vit_base_a_pretraining_test2 \
-# trainer=ddp_unused \
-# trainer.devices=8 \
-# trainer.num_nodes=8 \
-# configs.task="pretrain" \
-# configs.model_name="mae_vit_base_patch16" \
-# configs.input_size=224 \
-# configs.lr=1e-5 \
-# configs.weight_decay=5e-2 \
-# trainer.accumulate_grad_batches=1 \
-# configs.train_batch_size=2 \
-# configs.test_batch_size=4 \
-# configs.train_num_workers=16 \
-# configs.test_num_workers=16 \
-# trainer.gradient_clip_val=2.0 \
-# configs.scheduler="cosine_step" \
-# configs.lr_interval="step" \
-# configs.warmup_steps=20 \
-# trainer.max_epochs=800 \
-# configs.seq_length=32 \
-# configs.deltas_reg_weight=0.0 \
-# configs.random_frames=True \
-# configs.rgb_deltas=True \
-# configs.mean_deltas=True \
-# configs.num_classes=174 \
-# configs.vocab_size=256 \
-# configs.dataset_type="video" \
-# trainer.limit_train_batches=50000000 \
-# trainer.limit_val_batches=5000000 \
-# callbacks.model_checkpoint.every_n_epochs=1 \
-# configs.training_type="ucf101_gaussian_save-images-z_no-mask_random-frames" \
-# configs.load_strict=False \
-# configs.mask_ratio=0.95
-
-
-
-
-
-
-
-
-
-
-
-
-# python chewbacca/train.py -m \
-# --config-name gmae_ema.yaml \
-# hydra/launcher=submitit_slurm \
-# launcher=slurm_x \
-# model._target_="chewbacca.models.gmae.GMAELitModule" \
-# datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
-# task_name=vit_base_a_pretraining_test3 \
-# trainer=ddp_unused \
-# trainer.devices=8 \
-# trainer.num_nodes=1 \
-# configs.task="pretrain" \
-# configs.model_name="mae_vit_base_patch16" \
-# configs.input_size=224 \
-# configs.lr=1e-5 \
-# configs.weight_decay=5e-2 \
-# trainer.accumulate_grad_batches=1 \
-# configs.train_batch_size=2 \
-# configs.test_batch_size=4 \
-# configs.train_num_workers=16 \
-# configs.test_num_workers=16 \
-# trainer.gradient_clip_val=2.0 \
-# configs.scheduler="cosine_step" \
-# configs.lr_interval="step" \
-# configs.warmup_steps=2000 \
-# trainer.max_epochs=800 \
-# configs.seq_length=32 \
-# configs.deltas_reg_weight=0.0 \
-# configs.random_frames=True \
-# configs.rgb_deltas=True \
-# configs.mean_deltas=True \
-# configs.num_classes=174 \
-# configs.vocab_size=256 \
-# configs.dataset_type="video" \
-# trainer.limit_train_batches=50000000 \
-# trainer.limit_val_batches=5000000 \
-# callbacks.model_checkpoint.every_n_epochs=1 \
-# configs.training_type="ucf101_gaussian_save-images-z_no-mask_random-frames" \
-# configs.load_strict=False \
-# configs.mask_ratio=0.95
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# python chewbacca/train.py -m \
-# --config-name gmae_ema.yaml \
-# hydra/launcher=submitit_slurm \
-# launcher=slurm_x \
-# model._target_="chewbacca.models.gmae.GMAELitModule" \
-# datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
-# task_name=vit_base_a_pretraining_test4 \
+# task_name=a_vit_base_pretraining_test0 \
 # trainer=ddp_unused \
 # trainer.devices=8 \
 # trainer.num_nodes=8 \
@@ -193,10 +37,17 @@
 # configs.dataset_type="video" \
 # trainer.limit_train_batches=50000000 \
 # trainer.limit_val_batches=5000000 \
-# callbacks.model_checkpoint.every_n_epochs=1 \
+# callbacks.model_checkpoint.every_n_epochs=10 \
 # configs.training_type="ucf101_gaussian_save-images-z_no-mask_random-frames" \
 # configs.load_strict=False \
 # configs.mask_ratio=0.95
+
+
+
+
+
+
+
 
 
 
@@ -215,14 +66,14 @@ hydra/launcher=submitit_slurm \
 launcher=slurm_x \
 model._target_="chewbacca.models.gmae.GMAELitModule" \
 datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
-task_name=a_vit_base_pretraining_test0 \
+task_name=a_vit_base_pretraining_test1 \
 trainer=ddp_unused \
 trainer.devices=8 \
 trainer.num_nodes=8 \
 configs.task="pretrain" \
 configs.model_name="mae_vit_base_patch16" \
 configs.input_size=224 \
-configs.lr=1e-5 \
+configs.lr=1e-3,4e-4,1e-4 \
 configs.weight_decay=5e-2 \
 trainer.accumulate_grad_batches=1 \
 configs.train_batch_size=2 \
@@ -239,7 +90,7 @@ configs.deltas_reg_weight=0.0 \
 configs.random_frames=True \
 configs.rgb_deltas=True \
 configs.mean_deltas=True \
-configs.num_classes=174 \
+configs.num_classes=200 \
 configs.vocab_size=256 \
 configs.dataset_type="video" \
 trainer.limit_train_batches=50000000 \
@@ -257,20 +108,24 @@ configs.mask_ratio=0.95
 
 
 
+
+
+
+
 # python chewbacca/train.py -m \
 # --config-name gmae_ema.yaml \
 # hydra/launcher=submitit_slurm \
 # launcher=slurm_x \
 # model._target_="chewbacca.models.gmae.GMAELitModule" \
 # datamodule._target_="chewbacca.datamodules.video_datamodule.VideoDataModule" \
-# task_name=vit_large_a_pretraining_test1 \
+# task_name=a_vit_base_pretraining_test2 \
 # trainer=ddp_unused \
 # trainer.devices=8 \
 # trainer.num_nodes=8 \
 # configs.task="pretrain" \
-# configs.model_name="mae_vit_large_patch16" \
+# configs.model_name="mae_vit_base_patch16" \
 # configs.input_size=224 \
-# configs.lr=1e-4 \
+# configs.lr=1e-3 \
 # configs.weight_decay=5e-2 \
 # trainer.accumulate_grad_batches=1 \
 # configs.train_batch_size=2 \
@@ -280,19 +135,19 @@ configs.mask_ratio=0.95
 # trainer.gradient_clip_val=2.0 \
 # configs.scheduler="cosine_step" \
 # configs.lr_interval="step" \
-# configs.warmup_steps=20 \
+# configs.warmup_steps=2000 \
 # trainer.max_epochs=800 \
 # configs.seq_length=32 \
 # configs.deltas_reg_weight=0.0 \
 # configs.random_frames=True \
 # configs.rgb_deltas=True \
 # configs.mean_deltas=True \
-# configs.num_classes=174 \
+# configs.num_classes=200 \
 # configs.vocab_size=256 \
 # configs.dataset_type="video" \
 # trainer.limit_train_batches=50000000 \
 # trainer.limit_val_batches=5000000 \
-# callbacks.model_checkpoint.every_n_epochs=1 \
+# callbacks.model_checkpoint.every_n_epochs=10 \
 # configs.training_type="ucf101_gaussian_save-images-z_no-mask_random-frames" \
 # configs.load_strict=False \
 # configs.mask_ratio=0.95
